383fd312554d908fb4ea4beef29611bedb99d27e
Douglas Hsu <douglas.g.hsu@gmail.com>
2023-08-18 21:20:13 +0800

    first attempt at models

---
+++ app/models/movie.rb
@@ -0,0 +1,8 @@
+class Movie < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+
+  has_many :bookmarks, dependent: :restrict_with_exception
+  has_many :lists, through: :bookmarks
+end
