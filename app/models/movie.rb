62052936fb24e27ec4013056338331d1f2250529
Douglas Hsu <douglas.g.hsu@gmail.com>
2023-08-18 21:31:21 +0800

    Prepare rails app with external specs

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,6 @@ class Movie < ApplicationRecord
   validates :overview, presence: true
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 
-  has_many :bookmarks, dependent: :restrict_with_exception
+  has_many :bookmarks, dependent: :destroy
   has_many :lists, through: :bookmarks
 end
